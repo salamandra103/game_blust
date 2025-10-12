54d2061763bbb63ef39676bdef3c24865671be3e
Vladimir Skrepkov <salamandra103@gmail.com>
2025-10-12 17:03:21 +0300

    create helpers for generate board

--- utils/helpers.js
+++ utils/helpers.js
@@ -1,11 +1,17 @@
 
-const top = [-1, 0]
-const bottom = [1, 0]
-const left = [0, -1]
-const right = [0, 1]
+let maxRow = maxCol = 3;
+
+const directions = {
+    top: [-1, 0],
+    bottom: [1, 0],
+    left: [0, -1],
+    right: [0, 1],
+}
 
 function generateBoard(n) {
     let board = []
+
+    maxRow = maxCol = n;
 
     for (let i = 0; i < n; i++) {
         board.push([])
@@ -22,40 +28,67 @@ function generateBoard(n) {
 function removeSimilarTitle(board, position) {
     const [x, y] = position
 
-    let siblintTitle = getSiblingTitle(board, [x, y]);
+    getSiblingTitle(board, [x, y]);
     debugger
 }
 
 function getSiblingTitle(board, position) {
     const [x, y] = position
-    const allResolvedPosition = [position];
-    const currentTitle = board[x][y]
+    // const currentTitle = board[x][y]
+    // let siblingCount = 0;
 
-    const findingTitleTop = (board[x + top[0]][y + top[1]]) || 0
-    const findingTitleBottom = (board[x + bottom[0]][y + bottom[1]]) || 0
-    const findingTitleLeft = (board[x + left[0]][y + left[1]]) || 0
-    const findingTitleRight = (board[x + right[0]][y + right[1]]) || 0
+    // const findingTitleTop = isValidSibling(x + top[0], y + top[1]) ? board[x + top[0]][y + top[1]] : 0
+    // const findingTitleBottom = isValidSibling(x + bottom[0], y + bottom[1]) ? board[x + bottom[0]][y + bottom[1]] : 0
+    // const findingTitleLeft = isValidSibling(x + left[0], y + left[1]) ? board[x + left[0]][y + left[1]] : 0
+    // const findingTitleRight = isValidSibling(x + right[0], y + right[1]) ? board[x + right[0]][y + right[1]] : 0
 
-    switch (currentTitle) {
-        case findingTitleTop:
-            allResolvedPosition.push(getSiblingTitle(board, [x + top[0], y + top[1]]))
-            break;
-        case findingTitleBottom:
-            allResolvedPosition.push(getSiblingTitle(board, [x + bottom[0], y + bottom[1]]))
-            break;
-        case findingTitleLeft:
-            allResolvedPosition.push(getSiblingTitle(board, [x + left[0], y + left[1]]))
+    const queue = []
 
-            break;
-        case findingTitleRight:
-            allResolvedPosition.push(getSiblingTitle(board, [x + right[0], y + right[1]]))
+    for (let direction in directions) {
+        const newX = x + direction[0];
+        const newY = y + direction[1];
+        const siblingTitle = isValidSibling(newX, newY) ? board[newX][newY] : null
 
-            break;
-        default:
-            return position;
+        if (siblingTitle !== null) {
+            queue.push([newX, newY])
+        }
     }
 
+    while (queue.length) {
+        const value = queue.shift()
+    }
 
+    // switch (currentTitle) {
+    //     case findingTitleTop:
+    //         board[x][y] = 'yes'
+    //         siblingCount++;
+    //         getSiblingTitle(board, [x + top[0], y + top[1]]);
+    //         break;
+    //     case findingTitleBottom:
+    //         board[x][y] = 'yes'
+    //         siblingCount++;
+    //         getSiblingTitle(board, [x + bottom[0], y + bottom[1]]);
+    //         break;
+    //     case findingTitleLeft:
+    //         board[x][y] = 'yes'
+    //         siblingCount++;
+    //         getSiblingTitle(board, [x + left[0], y + left[1]]);
+    //         break;
+    //     case findingTitleRight:
+    //         board[x][y] = 'yes'
+    //         siblingCount++;
+    //         getSiblingTitle(board, [x + right[0], y + right[1]]);
+    //         break;
+    //     default:
+    //         if (siblingCount) {
+    //             board[x][y] = 'yes';
+    //         }
+    //         break;
+    // }
+}
+
+function isValidSibling(row, col) {
+    return row >= 0 && row < maxRow && col >= 0 && col < maxCol
 }
 
 // const board = generateBoard(9)
